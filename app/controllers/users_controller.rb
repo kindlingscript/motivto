c2a67e6c2668b20749f1608928f152f4ca386803
Rebecca Taylor <inflamedmuse@gmail.com>
2016-01-26 01:30:40 +0000

    restricted profile access to only the user who created it

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,17 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_current_user_to_own_profile, :only => [:show]
 
 	def show
 		@user = User.find(params[:id])
 	end
+
+  private
+
+  def require_current_user_to_own_profile
+    if current_user.id.to_s != params[:id]
+      render :text => "Unauthorized", :status => :unauthorized
+    end
+  end
 
 end
